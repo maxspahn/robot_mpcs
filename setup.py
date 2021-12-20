0c9485c972d442d52c2891a9ef45f32411ff1390
Max <m.spahn@tudelft.nl>
2021-12-20 16:39:37 +0100

    Adds shutil and glob in requirements.

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["numpy", "casadi"],
+    install_requires=["numpy", "casadi", "shutil", "glob"],
     python_requires=">=3.6",
 )
