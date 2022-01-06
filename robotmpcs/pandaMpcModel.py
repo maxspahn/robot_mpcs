3697e0e94a8a71163285f728003a375d7130ab92
Max <m.spahn@tudelft.nl>
2022-01-06 15:38:14 +0100

    Adds panda robot.

---
+++ robotmpcs/pandaMpcModel.py
@@ -0,0 +1,11 @@
+from robotmpcs.mpcModel import MpcModel
+from forwardkinematics.fksCommon.fk_creator import FkCreator
+
+
+class PandaMpcModel(MpcModel):
+
+    def __init__(self, m, N):
+        n = 7
+        super().__init__(m, n, N)
+        self._fk = FkCreator('panda', n).fk()
+
