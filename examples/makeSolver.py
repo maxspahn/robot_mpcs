2f9991c0189719d96c9382c64e9807d3ffd4ff95
Alexander <alexanderschwarz.ulm@gmail.com>
2022-06-12 22:53:55 +0200

    tuning of mpc parameters as well as test for pandampc

--- examples/makeSolver.py
+++ examples/makeSolver.py
@@ -1,20 +1,23 @@
 import os
-
+import sys
+sys.path.insert(0, '/Users/Alex/develop/forces_pro_client')
 from robotmpcs.models.pointRobotMpcModel import PointRobotMpcModel
 from robotmpcs.models.planarArmMpcModel import PlanarArmMpcModel
 from robotmpcs.models.diffDriveMpcModel import DiffDriveMpcModel
+from robotmpcs.models.pandaMpcModel import PandaMpcModel
 
 
 def main():
     N = 10
-    m = 2
+    m = 3
     dt = 0.01
     #mpcModel = PlanarArmMpcModel(m, N, 4)
-    mpcModel = PointRobotMpcModel(m, N)
+    #mpcModel = PointRobotMpcModel(m, N)
     #mpcModel = DiffDriveMpcModel(2, N)
+    mpcModel = PandaMpcModel(m ,N)
     mpcModel.setDt(dt)
     #mpcModel.setSlack()
-    mpcModel.setObstacles(1, 2)
+    #mpcModel.setObstacles(1, 2)
     mpcModel.setModel()
     mpcModel.setCodeoptions()
     path_to_solvers = os.path.dirname(os.path.abspath(__file__)) + '/solvers/'
