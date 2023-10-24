88984a894279f4808e651c5f23baa4f68eb45302
Max Spahn <m.spahn@tudelft.nl>
2023-10-24 21:31:18 +0200

    ft[fsd]: Adds running example with boxer and free space decomposition.

--- examples/boxer_example.py
+++ examples/boxer_example.py
@@ -1,19 +1,23 @@
 import sys
+from typing import List
 import os
 import numpy as np
 import gymnasium as gym
-import pybullet as p
 from urdfenvs.robots.generic_urdf.generic_diff_drive_robot import GenericDiffDriveRobot
 from urdfenvs.sensors.lidar import Lidar
-from urdfenvs.sensors.free_space_decomposition import FreeSpaceDecompositionSensor
 from mpscenes.obstacles.sphere_obstacle import SphereObstacle
 from mpscenes.goals.goal_composition import GoalComposition
 from urdfenvs.urdf_common.urdf_env import UrdfEnv
 from mpc_example import MpcExample
-from utils import visualize_constraints
+from robotmpcs.models.utils.free_space_decomposition import FreeSpaceDecomposition
+from robotmpcs.models.utils.utils import visualize_constraints_in_pybullet
 
 
 class BoxerMpcExample(MpcExample):
+
+    def __init__(self, config_file_name: str):
+        super().__init__(config_file_name)
+        self._fsd = FreeSpaceDecomposition(number_constraints=5, max_radius=5)
 
     def initialize_environment(self):
 
@@ -72,11 +76,36 @@ class BoxerMpcExample(MpcExample):
         for i in range(self._config['mpc']['time_horizon']):
             self._env.add_visualization(size=[self._r_body, 0.1])
 
+    def compute_constraints(self, robot_state: np.ndarray, lidar_obs: np.ndarray) -> List:
+        angle = robot_state[2]
+        rot_matrix = np.array([
+                [np.cos(angle), -np.sin(angle)], 
+                [np.sin(angle), np.cos(angle)],
+        ])
+
+        position_lidar = np.dot(rot_matrix, np.array([0.4, 0.0])) + robot_state[0:2]
+        number_rays = lidar_obs.shape[0]//2
+        lidar_observation = lidar_obs.reshape((number_rays, 2))
+        lidar_position = np.array([position_lidar[0], position_lidar[1], 0.02])
+        relative_positions = np.concatenate(
+            (
+                np.reshape(lidar_observation, (number_rays, 2)),
+                np.zeros((number_rays, 1)),
+            ),
+            axis=1,
+        )
+        absolute_positions = relative_positions + np.repeat(
+            lidar_position[np.newaxis, :], number_rays, axis=0
+        )
+
+        self._fsd.set_position(lidar_position)
+        self._fsd.compute_constraints(absolute_positions)
+        return list(self._fsd.asdict().values())
+
 
 
     def run(self):
         # add sensor
-        val = 40
         number_lidar_rays = 64
         lidar = Lidar(
                 "ee_joint",
@@ -85,17 +114,7 @@ class BoxerMpcExample(MpcExample):
                 plotting_interval=1,
                 angle_limits=np.array([-np.pi + np.pi/8, -np.pi/8]),
         )
-        fsd_sensor = FreeSpaceDecompositionSensor(
-                "ee_joint",
-                max_radius= 5,
-                number_constraints=5,
-                nb_rays=number_lidar_rays,
-                angle_limits=np.array([-np.pi + np.pi/8, -np.pi/8]),
-                plotting_interval=10, 
-                plotting_interval_fsd=10
-        )
         self._env.add_sensor(lidar, [0])
-        self._env.add_sensor(fsd_sensor, [0])
         self._env.set_spaces()
 
         q0 = np.median(self._limits, axis = 1)
@@ -109,14 +128,12 @@ class BoxerMpcExample(MpcExample):
             q = ob['robot_0']['joint_state']['position']
             qdot = ob['robot_0']['joint_state']['velocity']
             vel = np.array((ob['robot_0']['joint_state']['forward_velocity'], qdot[2]), dtype=float)
-            fsd_obs = ob['robot_0']["FreeSpaceDecompSensor"]
+            lidar_obs = ob['robot_0']['LidarSensor']
 
+            linear_constraints = self.compute_constraints(q, lidar_obs)
 
-            
-            linear_constraints = list(fsd_obs.values())
             self._planner.setLinearConstraints(linear_constraints, r_body=self._r_body)
             action,output = self._planner.computeAction(q, qdot, vel)
-            print(action)
             plan = []
             for key in output:
                 plan.append(np.concatenate([output[key][:2],np.zeros(1)]))
@@ -124,6 +141,7 @@ class BoxerMpcExample(MpcExample):
             if self.check_goal_reaching(ob):
                 print("goal reached")
                 break
+            visualize_constraints_in_pybullet(self._fsd, 0.02)
             self._env.update_visualizations(plan)
 
     def check_goal_reaching(self, ob):
